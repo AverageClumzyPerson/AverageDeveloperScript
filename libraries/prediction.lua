e472c5ae5866c75d4baace9cd1b6c73689de6f89
AverageClumzyPerson <tigerfishgmakaojd@gmail.com>
2026-06-20 09:12:40 -0500

    Update prediction.lua

--- libraries/prediction.lua
+++ libraries/prediction.lua
@@ -1,7 +1,6 @@
 --This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.
 local module = {}
 local eps = 1e-9
-
 local function isZero(d)
 	return (d > -eps and d < eps)
 end
@@ -11,22 +10,31 @@ local function cuberoot(x)
 end
 
 local function solveQuadric(c0, c1, c2)
+	local s0, s1
+
 	local p, q, D
+
 	p = c1 / (2 * c0)
 	q = c2 / c0
 	D = p * p - q
 
 	if isZero(D) then
-		return -p
+		s0 = -p
+		return s0
 	elseif (D < 0) then
 		return
 	else 
 		local sqrt_D = math.sqrt(D)
-		return sqrt_D - p, -sqrt_D - p
+
+		s0 = sqrt_D - p
+		s1 = -sqrt_D - p
+		return s0, s1
 	end
 end
 
 local function solveCubic(c0, c1, c2, c3)
+	local s0, s1, s2
+
 	local num, sub
 	local A, B, C
 	local sq_A, p, q
@@ -42,8 +50,6 @@ local function solveCubic(c0, c1, c2, c3)
 
 	cb_p = p * p * p
 	D = q * q + cb_p
-
-	local s0, s1, s2
 
 	if isZero(D) then
 		if isZero(q) then 
@@ -82,27 +88,8 @@ local function solveCubic(c0, c1, c2, c3)
 end
 
 function module.solveQuartic(c0, c1, c2, c3, c4)
-	if isZero(c0) then
-		if isZero(c1) then
-			if isZero(c2) then
-				if isZero(c3) then return {} end
-				return {-c4 / c3}
-			end
-			local r1, r2 = solveQuadric(c2, c3, c4)
-			local res = {}
-			if r1 then table.insert(res, r1) end
-			if r2 then table.insert(res, r2) end
-			return res
-		end
-		local r1, r2, r3 = solveCubic(c1, c2, c3, c4)
-		local res = {}
-		if r1 then table.insert(res, r1) end
-		if r2 then table.insert(res, r2) end
-		if r3 then table.insert(res, r3) end
-		return res
-	end
-
 	local s0, s1, s2, s3
+
 	local coeffs = {}
 	local z, u, v, sub
 	local A, B, C, D
@@ -145,100 +132,99 @@ function module.solveQuartic(c0, c1, c2, c3, c4)
 		elseif (u > 0) then
 			u = math.sqrt(u)
 		else
-			return {}
+			return
 		end
 		if isZero(v) then
 			v = 0
 		elseif (v > 0) then
 			v = math.sqrt(v)
 		else
-			return {}
+			return
 		end
 
 		coeffs[2] = z - u
 		coeffs[1] = q < 0 and -v or v
 		coeffs[0] = 1
 
-		local results1 = {solveQuadric(coeffs[0], coeffs[1], coeffs[2])}
-		num = #results1
-		s0, s1 = results1[1], results1[2]
+		do
+			local results = {solveQuadric(coeffs[0], coeffs[1], coeffs[2])}
+			num = #results
+			s0, s1 = results[1], results[2]
+		end
 
 		coeffs[2] = z + u
 		coeffs[1] = q < 0 and v or -v
 		coeffs[0] = 1
 
-		local results2 = {solveQuadric(coeffs[0], coeffs[1], coeffs[2])}
 		if (num == 0) then
-			num = num + #results2
-			s0, s1 = results2[1], results2[2]
-		elseif (num == 1) then
-			num = num + #results2
-			s1, s2 = results2[1], results2[2]
-		elseif (num == 2) then
-			num = num + #results2
-			s2, s3 = results2[1], results2[2]
+			local results = {solveQuadric(coeffs[0], coeffs[1], coeffs[2])}
+			num = num + #results
+			s0, s1 = results[1], results[2]
+		end
+		if (num == 1) then
+			local results = {solveQuadric(coeffs[0], coeffs[1], coeffs[2])}
+			num = num + #results
+			s1, s2 = results[1], results[2]
+		end
+		if (num == 2) then
+			local results = {solveQuadric(coeffs[0], coeffs[1], coeffs[2])}
+			num = num + #results
+			s2, s3 = results[1], results[2]
 		end
 	end
 
 	sub = 0.25 * A
 
-	local res = {}
-	if (num > 0 and s0) then table.insert(res, s0 - sub) end
-	if (num > 1 and s1) then table.insert(res, s1 - sub) end
-	if (num > 2 and s2) then table.insert(res, s2 - sub) end
-	if (num > 3 and s3) then table.insert(res, s3 - sub) end
+	if (num > 0) then s0 = s0 - sub end
+	if (num > 1) then s1 = s1 - sub end
+	if (num > 2) then s2 = s2 - sub end
+	if (num > 3) then s3 = s3 - sub end
 
-	return res
+	return {s3, s2, s1, s0}
 end
 
-function module.SolveTrajectory(origin, projectileSpeed, gravity, targetPos, targetVelocity, playerGravity, playerHeight, ping, targetGravity)
-	ping = ping or 0
-	targetGravity = targetGravity or 0
-	
-	local predictedTargetPos = targetPos + targetVelocity * ping
-	
-	local disp = predictedTargetPos - origin
+function module.SolveTrajectory(origin, projectileSpeed, gravity, targetPos, targetVelocity, playerGravity, playerHeight, params)
+	local disp = targetPos - origin
 	local p, q, r = targetVelocity.X, targetVelocity.Y, targetVelocity.Z
 	local h, j, k = disp.X, disp.Y, disp.Z
-	
-	local g_eff = gravity - targetGravity
-	local l = -0.5 * g_eff
+	local l = -.5 * (gravity - (playerGravity or 0))
 
-	local c0 = l * l
-	local c1 = -2 * q * l
-	local c2 = q * q - 2 * j * l - projectileSpeed * projectileSpeed + p * p + r * r
-	local c3 = 2 * j * q + 2 * h * p + 2 * k * r
-	local c4 = j * j + h * h + k * k
-
-	local solutions = module.solveQuartic(c0, c1, c2, c3, c4)
+	local solutions = module.solveQuartic(
+		l*l,
+		-2*q*l,
+		q*q - 2*j*l - projectileSpeed*projectileSpeed + p*p + r*r,
+		2*j*q + 2*h*p + 2*k*r,
+		j*j + h*h + k*k
+	)
 	
 	local function linearFallback()
 		local t = disp.Magnitude / projectileSpeed
-		if t <= 0 then return predictedTargetPos end
-		local d = (h + p * t) / t
-		local e = (j + q * t) / t + 0.5 * gravity * t
-		local f = (k + r * t) / t
+		if t <= 0 then return targetPos end
+		local d = (h + p*t)/t
+		local e = (j + q*t)/t + 0.5 * gravity * t
+		local f = (k + r*t)/t
 		return origin + Vector3.new(d, e, f)
 	end
 
-	if solutions and #solutions > 0 then
+	if solutions then
 		local posRoots = {}
-		for _, v in ipairs(solutions) do
-			if v > 0.001 then
+		for _, v in solutions do
+			if v > 0 then
 				table.insert(posRoots, v)
 			end
 		end
 		table.sort(posRoots)
 
+		local bestResult = nil
 		if #posRoots > 0 then
 			local t = posRoots[1]
-			local aimX = (h + p * t) / t
-			local aimY = (j + q * t - l * t * t) / t
-			local aimZ = (k + r * t) / t
-			
-			local hitboxOffset = playerHeight and (playerHeight * 0.6) or 1.5
-			
-			return origin + Vector3.new(aimX, aimY + hitboxOffset, aimZ)
+			local d = (h + p*t)/t
+			local e = (j + q*t - l*t*t)/t
+			local f = (k + r*t)/t
+			bestResult = origin + Vector3.new(d, e, f)
+		end
+		if bestResult then
+			return bestResult
 		end
 	end
 
